82e5976f2a88c3da595a222a63166df863735947
kwiaciu <bkwiatek3@gmail.com>
2021-01-06 17:17:38 +0100

    build

---
+++ client/build/precache-manifest.5f74a8808eb9196c6320b3a917dcc64e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d08771e92bb1f69bff4765e1787049",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0afdac4490f321143fb",
+    "url": "/static/css/main.574debe9.chunk.css"
+  },
+  {
+    "revision": "43280093b370ad4cf72a",
+    "url": "/static/js/2.5e70ae42.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5e70ae42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0afdac4490f321143fb",
+    "url": "/static/js/main.624a7b7c.chunk.js"
+  },
+  {
+    "revision": "03037a903628a371ede4",
+    "url": "/static/js/runtime-main.d75105de.js"
+  }
+]);
