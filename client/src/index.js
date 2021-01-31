a26ffe0287f1e952db5447f4e525e48290bdfd13
kwiaciu <bkwiatek3@gmail.com>
2021-01-31 20:48:38 +0100

    created themes file

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import { ChakraProvider } from "@chakra-ui/react"
 import { Auth0Provider } from "@auth0/auth0-react";
 import config from "./auth_config.json";
 import history from "./utils/history";
-
+import theme from "./themes"
 const onRedirectCallback = (appState) => {
   history.push(
     appState && appState.returnTo ? appState.returnTo : window.location.pathname
@@ -23,7 +23,7 @@ ReactDOM.render(
       redirectUri={window.location.origin}
       onRedirectCallback={onRedirectCallback}
     >
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
 
         <App />
       </ChakraProvider>
---
+++ client/src/themes.js
@@ -0,0 +1,23 @@
+import { extendTheme } from "@chakra-ui/react"
+
+
+const theme = extendTheme({
+  components: {
+    Container: {
+      // 1. We can update the base styles
+      baseStyle: {
+        "max-width": "80ch", // Normally, it is "semibold"
+      },
+    }
+  },
+
+  colors: {
+    brand: {
+      100: "#f7fafc",
+      // ...
+      900: "#1a202c",
+    },
+  },
+})
+
+export default theme;
