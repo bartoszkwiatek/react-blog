4ca0a2b65d458ebc69a64b36e35c9e84eb2c644f
kwiaciu <bkwiatek3@gmail.com>
2021-03-06 23:36:55 +0100

    changes to routes permissions

--- routes.js
+++ routes.js
@@ -1,24 +1,31 @@
-const router = require('express').Router();
-const checkJwt = require('./checkJwt');
-const jwtAuthz = require('express-jwt-authz');
+const router = require('express').Router()
+const checkJwt = require('./checkJwt')
+const jwtAuthz = require('express-jwt-authz')
 
-let PostModel = require('./post.model');
+let PostModel = require('./post.model')
 
+// get all posts
 router.route('/').get((req, res) => {
-  PostModel.find().sort({ createdAt: -1 })
-    .then(posts => res.json(posts))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
+  PostModel.find()
+    .sort({ createdAt: -1 })
+    .then((posts) => res.json(posts))
+    .catch((err) => res.status(400).json('Error: ' + err))
+})
 
+//get post by id
+router.route('/:id').get((req, res) => {
+  PostModel.findById(req.params.id)
+    .then((exercise) => res.json(exercise))
+    .catch((err) => res.status(400).json('Error: ' + err))
+})
 
 // new post
 router.route('/').post(checkJwt, jwtAuthz(['add:posts']), async (req, res) => {
-  // const username = req.body.username;
-  const title = req.body.title;
-  const shortContent = req.body.shortContent;
-  const longContent = req.body.longContent;
-  const author = req.body.author;
-  const createdAt = Date.now();
+  const title = req.body.title
+  const shortContent = req.body.shortContent
+  const longContent = req.body.longContent
+  const author = req.body.author
+  const createdAt = Date.now()
 
   const newPost = new PostModel({
     title,
@@ -26,43 +33,40 @@ router.route('/').post(checkJwt, jwtAuthz(['add:posts']), async (req, res) => {
     longContent,
     author,
     createdAt,
-  });
+  })
 
-  newPost.save()
+  newPost
+    .save()
     .then(() => res.json('Post added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
-
-//get post by id
-router.route('/:id').get((req, res) => {
-  PostModel.findById(req.params.id)
-    .then(exercise => res.json(exercise))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
-
+    .catch((err) => res.status(400).json('Error: ' + err))
+})
 
 // delete post
-router.route('/:id').delete((req, res) => {
-  PostModel.findByIdAndDelete(req.params.id)
-    .then(() => res.json('Post deleted.'))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
-
+router
+  .route('/:id')
+  .delete(checkJwt, jwtAuthz(['edit:posts']), async (req, res) => {
+    PostModel.find(req.params.id)
+      .then(() => res.json('Post deleted.'))
+      .catch((err) => res.status(400).json('Error: ' + err))
+  })
 
 // update post
-router.route('/:id').put((req, res) => {
-  PostModel.findById(req.params.id)
-    .then(post => {
-      post.title = req.body.title;
-      post.shortContent = req.body.shortContent;
-      post.longContent = req.body.longContent;
-      post.createdAt = Date.parse(req.body.createdAt);
+router
+  .route('/:id')
+  .put(checkJwt, jwtAuthz(['edit:posts']), async (req, res) => {
+    PostModel.findByIdAndUpdate(req.params.id)
+      .then((post) => {
+        post.title = req.body.title
+        post.shortContent = req.body.shortContent
+        post.longContent = req.body.longContent
+        post.createdAt = Date.parse(req.body.createdAt)
 
-      post.save()
-        .then(() => res.json('Post updated!'))
-        .catch(err => res.status(400).json('Error: ' + err));
-    })
-    .catch(err => res.status(400).json('Error: ' + err));
-});
+        post
+          .save()
+          .then(() => res.json('Post updated!'))
+          .catch((err) => res.status(400).json('Error: ' + err))
+      })
+      .catch((err) => res.status(400).json('Error: ' + err))
+  })
 
-module.exports = router;
+module.exports = router
