2deab0cae792fa19075ca971ec2e325cddce93d3
kwiaciu <bkwiatek3@gmail.com>
2021-03-08 22:31:26 +0100

    created fetching hook

---
+++ client/src/utils/useFetch.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+import { handleErrors } from '../utils/handleErrors'
+
+export const useFetch = (url, numberOfUpdates = 0) => {
+  const [error, setError] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    setIsLoaded(false)
+    fetch(url)
+      .then(handleErrors)
+      .then((res) => res.json())
+      .then((result) => {
+        setItems(result)
+        setIsLoaded(true)
+      })
+      .catch((error) => {
+        setError(error)
+        setIsLoaded(true)
+      })
+    // console.log('useEffect custom hook here!', numberOfUpdates)
+  }, [url, numberOfUpdates])
+
+  return {
+    isLoaded,
+    items,
+    error,
+  }
+}
